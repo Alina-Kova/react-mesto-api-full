01e703ca78eab65e6afba39b12238ad1331e7dc5
Alina Vayer <lovering2007@mail.ru>
2021-06-28 22:18:59 +0300

    update

--- mesto-frontend/src/utils/api.js
+++ mesto-frontend/src/utils/api.js
@@ -118,7 +118,7 @@ export default class Api {
 export const api = new Api({
     baseUrl: 'https://api.alina.mesto.nomoredomains.monster',
 	headers: {
-        // authorization: `Bearer ${localStorage.getItem('jwt')}`,
+        authorization: `Bearer ${localStorage.getItem('jwt')}`,
 		'Content-Type': 'application/json'
 	}
 })
