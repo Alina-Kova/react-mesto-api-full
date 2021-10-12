ab86c30f020e05aa14c493f13a10941b209573fb
Alina Vayer <lovering2007@mail.ru>
2021-10-12 21:20:20 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ const { login, createUser } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
-// const { handleCors } = require('./middlewares/cors');
+const { handleCors } = require('./middlewares/cors');
+// const cookieParser = require('cookie-parser');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -29,31 +30,72 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    'https://alina.mesto.nomoredomains.monster',
-    'http://alina.mesto.nomoredomains.monster',
-    'https://api.alina.mesto.nomoredomains.monster',
-    'http://api.alina.mesto.nomoredomains.monster',
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:3000',
+//     'https://localhost:3000',
+//     'https://alina.mesto.nomoredomains.monster',
+//     'http://alina.mesto.nomoredomains.monster',
+//     // 'https://api.alina.mesto.nomoredomains.monster',
+//     // 'http://api.alina.mesto.nomoredomains.monster',
 
-  ],
-  credentials: true,
-  methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+//   ],
+//   credentials: true,
+//   methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+// //////////
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:3000',
+//     'https://alina.mesto.nomoredomains.monster',
+//     'http://alina.mesto.nomoredomains.monster',
+//   ],
+//   credentials: true,
+// };
+
+// // eslint-disable-next-line consistent-return
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (corsOptions.includes(origin)) {
+//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//     res.header('Access-Control-Allow-Origin', '*');
+//   }
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
+//   next();
+// });
+
+// app.options('*', cors());
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
 });
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(handleCors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(limiter);
+// app.use(cookieParser());
 
 app.use(requestLogger); // подключаем логгер запросов
 
